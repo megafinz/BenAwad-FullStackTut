6f34f7835876ce2575ff56e977a8e2a249a9e423
megafinz <megafinz@gmail.com>
2023-01-15 14:32:38 +0100

    Some Apollo logging

--- frontend/lib/apollo.ts
+++ frontend/lib/apollo.ts
@@ -1,13 +1,47 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+  from,
+  InMemoryCache
+} from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
+
+const consoleLink = new ApolloLink((operation, forward) => {
+  console.log(`[APOLLO] Starting request for '${operation.operationName}'`)
+  console.dir(operation.getContext())
+  return forward(operation).map(data => {
+    console.log(`[APOLLO] Ending request for '${operation.operationName}'`)
+    return data
+  })
+})
+
+const errorLink = onError(({ graphQLErrors: graphqlErrors, networkError }) => {
+  for (const { message, locations, path } of graphqlErrors || []) {
+    console.log(
+      `[APOLLO][GQL ERROR]: Message: ${message}, Location: ${locations}, Path: ${path}`
+    )
+  }
+  if (networkError) {
+    console.log(`[APOLLO][NET ERROR]: ${networkError}`)
+  }
+})
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
   credentials: 'include'
 })
 
+const links: ApolloLink[] = []
+
+if (process.env.NODE_ENV === 'development') {
+  links.push(consoleLink, errorLink)
+}
+
+links.push(httpLink)
+
 // TODO: URI from config
-// TODO: error link
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: httpLink
+  link: from(links)
 })
